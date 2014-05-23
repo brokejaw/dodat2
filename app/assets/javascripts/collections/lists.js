24341c98fa9d4553c3f13fe24cf9beed4ee0e2de
stephen brokaw <brokaw.stephen@gmail.com>
2014-05-23 09:48:58 -0700

    checking in

--- app/assets/javascripts/collections/lists.js
+++ app/assets/javascripts/collections/lists.js
@@ -1,8 +1,19 @@
 window.Dodat2.Collections.Lists = Backbone.Collection.extend({
-	model: Dodat2.Models.List
+	model: Dodat2.Models.List,
 	
 	url: function () {
-		return "api/boards/" + ??? + "lists";
+		//return "api/boards/" + this.board.get("id") + "lists";
+		return this.board.url() + "/comments";
 	},
 	
+	initialize: function (models, options) {
+		this.board = options.board;
+	}
+	
 });
+
+// example
+// var board = new Dodat2.Models.Board({ id: 3 })
+// var lists = new Dodat2.Collections.Lists([], {
+//	board: board
+// }); 
