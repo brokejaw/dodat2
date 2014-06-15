6da365b94ce1faf35a0885b5a977f887c360f883
stephen brokaw <brokaw.stephen@gmail.com>
2014-06-15 13:42:48 -0700

    header styling change

--- app/assets/javascripts/models/board.js
+++ app/assets/javascripts/models/board.js
@@ -1,17 +1,22 @@
 window.Dodat2.Models.Board = Backbone.Model.extend({
+	initialize: function() {
+		
+	},
+	
 	urlRoot: "/api/boards", 
+	
 	// server-side root location
 	// collections refer to this, models add the :id
 	
+	// overriding default. 
 	parse: function (json) {
+		
 		if (json.lists) {
 			this.lists().set(json.lists, { parse: true } );
 			delete json.lists;
 		}
 		return json;
 	},
-	// method automatically called when data returned by the server, fetch, save
-	// json == raw response object. 
 	
 	lists: function () {
 		if (!this._lists) {
