7fe21fe3732e4592edf616ba60070aa17eb2a81f
stephen brokaw <brokaw.stephen@gmail.com>
2014-08-29 21:50:13 -0700

    update signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,17 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-    
-      if user
-        login(user)
-        redirect_to root_url
-      else
-        flash.now[:errors] = ["Invalid Username or Password"]
-        render :new, status: 401
-      end
+    if user
+      login(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid Username or Password"]
+      render :new, status: 401
     end
+  end
     
-    def destroy
-      logout
-      redirect_to new_session_url
-    end
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,42 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Board.create!(:title => "Home", :user_id => 1)
-Board.create!(:title => "Work", :user_id => 1)
-Board.create!(:title => "School", :user_id => 1)
-List.create!(:title => "To Do", :board_id => 1)
-List.create!(:title => "Doing", :board_id => 1)
-List.create!(:title => "Done", :board_id => 1) 
-List.create!(:title => "To Do", :board_id => 2)
-List.create!(:title => "Doing", :board_id => 2)
-List.create!(:title => "Done", :board_id => 2) 
+Board.create!(:title => "Welcome", :user_id => 1)
+List.create!(:title => "The basics", :board_id => 1)
+Card.create!(:title => "Im a card!", :description => "..and I have details!", :list_id => 1)
+Card.create!(:title => "Cards have details", :description => "I'm a detail!", :list_id => 1)
+Card.create!(:title => "You can hover over me", :description => "storage", :list_id => 1)
+Card.create!(:title => "Click 'i' for details", :list_id => 1)
+Card.create!(:title => "..or trashcan to delete", :list_id => 1)
+
+
+List.create!(:title => "Advanced features", :board_id => 1)
+Card.create!(:title => "Clean the house", :description => "upstairs", :list_id => 2)
+Card.create!(:title => "Finish Painting", :description => "bathroom", :list_id => 2)
+Card.create!(:title => "Organize Basement", :description => "storage", :list_id => 2)
+
+List.create!(:title => "Blah blah", :board_id => 1)
+Card.create!(:title => "Clean the house", :description => "upstairs", :list_id => 1)
+Card.create!(:title => "Finish Painting", :description => "bathroom", :list_id => 2)
+Card.create!(:title => "Organize Basement", :description => "storage", :list_id => 3)
+
+Board.create!(:title => "Fitness", :user_id => 1)
+List.create!(:title => "Monday", :board_id => 2)
+Card.create!(:title => "Clean the house", :description => "upstairs", :list_id => 1)
+Card.create!(:title => "Finish Painting", :description => "bathroom", :list_id => 2)
+Card.create!(:title => "Organize Basement", :description => "storage", :list_id => 3)
+List.create!(:title => "Wendesday", :board_id => 2)
+List.create!(:title => "Friday", :board_id => 2) 
+List.create!(:title => "Saturday", :board_id => 2)
+Board.create!(:title => "Dinner Party Mashup", :user_id => 1)
+
+
+
+List.create!(:title => "Monday", :board_id => 2)
+List.create!(:title => "Wendesday", :board_id => 2)
+List.create!(:title => "Friday", :board_id => 2) 
+List.create!(:title => "Saturday", :board_id => 2) 
+
 List.create!(:title => "To Do", :board_id => 3)
 List.create!(:title => "Doing", :board_id => 3)
 List.create!(:title => "Done", :board_id => 3) 
