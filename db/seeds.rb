541ce326f26aac7db29014d9cec201dfe1207166
stephen brokaw <brokaw.stephen@gmail.com>
2014-11-06 13:28:26 -0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,34 +52,36 @@ Card.create!(:title => "1.5 miles, 7:45 pace", :description => "upstairs", :list
 Card.create!(:title => "Pushups, 6x15", :description => "bathroom", :list_id => 6)
 Card.create!(:title => "Planks, 4 x 1 min", :description => "storage", :list_id => 6)
 Card.create!(:title => "Airdyne, 10x1 min", :description => "storage", :list_id => 6)
+
+#list 7
 List.create!(:title => "Saturday", :board_id => 2)
 
 #board 3
 Board.create!(:title => "Dinner Party Mashup", :user_id => 1)
-#list 7
+#list 8
 List.create!(:title => "Drinkers", :board_id => 3)
-Card.create!(:title => "C. Bukowski", :description => "upstairs", :list_id => 7)
-Card.create!(:title => "C. Hitchens", :description => "bathroom", :list_id => 7)
-Card.create!(:title => "J. Cheever", :description => "storage", :list_id => 7)
-Card.create!(:title => "D. Parker", :description => "storage", :list_id => 7)
-Card.create!(:title => "J. Rhys", :description => "storage", :list_id => 7)
+Card.create!(:title => "C. Bukowski", :description => "upstairs", :list_id => 8)
+Card.create!(:title => "C. Hitchens", :description => "bathroom", :list_id => 8)
+Card.create!(:title => "J. Cheever", :description => "storage", :list_id => 8)
+Card.create!(:title => "D. Parker", :description => "storage", :list_id => 8)
+Card.create!(:title => "J. Rhys", :description => "storage", :list_id => 8)
 
 #list 8
 List.create!(:title => "Fighters", :board_id => 3)
-Card.create!(:title => "E. Hemingway", :list_id => 8)
-Card.create!(:title => "H.S. Thompson", :list_id => 8)
+Card.create!(:title => "E. Hemingway", :list_id => 9)
+Card.create!(:title => "H.S. Thompson", :list_id => 9)
 
 #list 9
 List.create!(:title => "Space-time", :board_id => 3) 
-Card.create!(:title => "U. Le Guin", :description => "upstairs", :list_id => 9)
-Card.create!(:title => "F. Herbert", :description => "bathroom", :list_id => 9)
-Card.create!(:title => "R. Bradbury", :description => "storage", :list_id => 9)
-Card.create!(:title => "R. Heinlein", :description => "storage", :list_id => 9)
-Card.create!(:title => "A. Clarke", :description => "storage", :list_id => 9)
-Card.create!(:title => "D. Adams", :description => "storage", :list_id => 9)
+Card.create!(:title => "U. Le Guin", :description => "upstairs", :list_id => 10)
+Card.create!(:title => "F. Herbert", :description => "bathroom", :list_id => 10)
+Card.create!(:title => "R. Bradbury", :description => "storage", :list_id => 10)
+Card.create!(:title => "R. Heinlein", :description => "storage", :list_id => 10)
+Card.create!(:title => "A. Clarke", :description => "storage", :list_id => 10)
+Card.create!(:title => "D. Adams", :description => "storage", :list_id => 10)
 
 #list 10
 List.create!(:title => "Poets", :board_id => 3)
 Card.create!(:title => "P. Larkin", :description => "this be the verse", :list_id => 10)
-Card.create!(:title => "P. Neruda", :list_id => 10)
-Card.create!(:title => "E. Dickinson", :list_id => 10)
+Card.create!(:title => "P. Neruda", :list_id => 11)
+Card.create!(:title => "E. Dickinson", :list_id => 11)
